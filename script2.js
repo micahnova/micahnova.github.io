1de8691e0972be98eef677aa0b993e182a6ff3b4
micahnova <128766585+micahnova@users.noreply.github.com>
2026-01-10 23:26:50 -0600

    shadow box info

--- script2.js
+++ script2.js
@@ -1,13 +1,28 @@
 
 const gallery = document.getElementById('gallery');
 
-// Sample data (replace with your own art collection)
+// Art collection with additional info
 let artCollection = [
-  { title: 'A-T Walker 3', img: 'assets/cat2025ATW3.jpg' },
-  { title: 'A-T Walker 2', img: 'assets/cat2024ATW2.jpg' },
-  { title: 'A-T Walker 1', img: 'assets/cat2023ATW1.jpg' },
-  { title: 'Space Car Blue Book 2', img: 'assets/cat2025SCBB2.jpg', description: 'and <b>Mochidays</b> by Mochiwei' },
-  // Add more items here...
+  {
+    title: 'A-T Walker 3',
+    img: 'assets/cat2025ATW3.jpg',
+    description: '2025<br> full color <br> 24 pages'
+  },
+  {
+    title: 'A-T Walker 2',
+    img: 'assets/cat2024ATW2.jpg',
+    description: '2024<br> full color <br> 24 pages'
+  },
+  {
+    title: 'A-T Walker 1',
+    img: 'assets/cat2023ATW1.jpg',
+    description: '2023<br> full color <br> 24 pages'
+  },
+  {
+    title: 'Space Car Blue Book 2',
+    img: 'assets/cat2025SCBB2.jpg',
+    description: '2025<br> split zine with Mochidays by Mochiwei<br> full color<br> 24 pages'
+  }
 ];
 
 // Sort by title alphabetically
@@ -56,7 +71,7 @@ function openLightbox(item) {
   lightboxImg.alt = item.title;
   lightboxInfo.innerHTML = `
     <h2>${item.title}</h2>
-    <p>${item.description || 'No additional info available.'}</p>
+    <p>${item.description.replace(/<br>/g, '<br>')}</p>
   `;
 }
 
